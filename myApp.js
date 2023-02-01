1628ab98e3607b168975a7e611838d23e49762af
Carlos <rvjosecarlos@gmail.com>
2023-01-31 19:37:12 -0600

    Commit 2: Tema 2, Hello Express como respuesta a solicitudes get

--- myApp.js
+++ myApp.js
@@ -4,9 +4,9 @@ let app = express();
 console.log("Hello World");
 
 app.get( '/', ( req, res )=>{
-    console.log('SOLICITUD',req);
-    console.log('RESPUESTA',res);
-    res.send('Hello Express');
+
+        res.send('Hello Express');
+    
 });
 
 module.exports = app;
