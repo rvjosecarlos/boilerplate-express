3f7969e743436335ed0c2b7cba3ed066d2662889
Carlos <rvjosecarlos@gmail.com>
2023-01-31 19:06:43 -0600

    Respues desde el servidor 'Express'

--- myApp.js
+++ myApp.js
@@ -3,4 +3,10 @@ let app = express();
 
 console.log("Hello World");
 
- module.exports = app;
+app.get( '/', ( req, res )=>{
+    console.log('SOLICITUD',req);
+    console.log('RESPUESTA',res);
+    res.send('Hello Express');
+});
+
+module.exports = app;
