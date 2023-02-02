426f90ea837f68e76383f3a64dd9af3080c513b5
Carlos <rvjosecarlos@gmail.com>
2023-02-01 18:56:13 -0600

    Se agrego la ruta relativa como primer parametro

--- myApp.js
+++ myApp.js
@@ -3,12 +3,14 @@ let app = express();
 
 console.log("Hello World");
 
+app.use( express.static( __dirname + '/public' ) );
+
 app.get( '/', ( req, res )=>{
 
         res.sendFile(`${__dirname}/views/index.html`);
     
 });
 
-app.use( express.static( path.join( __dirname, 'public' ) ) );
+app.use( '/public', express.static( __dirname + '/public' ) );
 
 module.exports = app;
